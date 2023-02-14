b7b4d8e53eba6f5a4abac8e0a31279eb265398d4
A-HK <kahani.0142@gmail.com>
2023-02-14 14:55:41 +0530

    added initial .cpp file

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World. I am PES1UG20CS054\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Successfully built and run\n";
+  return 0;
+}
