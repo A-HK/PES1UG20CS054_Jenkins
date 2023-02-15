0dc6eb721376eb01f41c42c33f507bf5169ca064
A-HK <kahani.0142@gmail.com>
2023-02-15 10:00:40 +0530

    added new cpp file for pipeline demo

---
+++ PES1UG20CS054.cpp
@@ -0,0 +1,10 @@
+//File: PES1UG20CS054.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "This is a demo CPP file to demonstrate that pipeline executes successfully\n";
+  cout << "By SRN: PES1UG20CS054";
+
+  return 0;
+}
